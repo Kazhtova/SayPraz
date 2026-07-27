f6a927a32e1a5dc99fde815723e59930b19b3a27
Kazhtova <kasshhhoktora@gmail.com>
2026-07-27 18:43:59 +0700

    Add Route Transaction

--- backend-api/routes/api.php
+++ backend-api/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AssetController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::apiResource('assets', AssetController::class);
     Route::apiResource('categories', CategoryController::class); 
+
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    Route::post('/transactions', [TransactionController::class, 'store']);
+    Route::patch('/transaction/{id}/status', [TransactionController::class, 'updateStatus']);
 });
