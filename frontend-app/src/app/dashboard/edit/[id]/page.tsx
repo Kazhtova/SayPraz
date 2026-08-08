3f4d6668c7b027e77702775dbe1f6ce59f25750f
Kazhtova <kasshhhoktora@gmail.com>
2026-08-08 07:45:44 +0700

    Fix View Dashboard Edit

--- frontend-app/src/app/dashboard/edit/[id]/page.tsx
+++ frontend-app/src/app/dashboard/edit/[id]/page.tsx
@@ -323,7 +323,7 @@ export default function EditAssetPage() {
               <div className="space-y-2">
                 <label className="text-xs font-semibold text-zinc-400 uppercase tracking-wider flex items-center gap-1.5">
                   <ImageIcon className="h-4 w-4 text-zinc-400" />
-                  Foto Aset <span className="text-zinc-500 font-normal lowercase">(opsional)</span>
+                  Foto Aset <span className="text-zinc-500 font-normal lowercase"></span>
                 </label>
 
                 {!imagePreview ? (
