a180df64a637df3378f53401b20cbdb871de2d4d
Kazhtova <kasshhhoktora@gmail.com>
2026-08-10 18:32:09 +0700

    Remodel Transactions model

--- backend-api/app/Models/Transaction.php
+++ backend-api/app/Models/Transaction.php
@@ -5,12 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use App\Enums\TransactionStatus;
 
 class Transaction extends Model
 {
     use HasFactory;
-    protected $fillable = ['asset_id', 'user_id', 'borrowed_date', 'expected_returned_date', 'actual_returned_date', 'status'];
+
+    protected $fillable = [
+        'asset_id', 
+        'user_id', 
+        'status',
+        'expected_returned_date', 
+        'borrowed_at', 
+        'returned_at', 
+    ];
+
+    /**
+     * Dapatkan aturan casting atribut.
+     */
+    protected function casts(): array
+    {
+        return [
+            'status'                 => TransactionStatus::class,
+            'expected_returned_date' => 'date',
+            'borrowed_at'            => 'datetime',
+            'returned_at'            => 'datetime',
+        ];
+    }
 
     public function asset(): BelongsTo
     {
