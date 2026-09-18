227a12acf6a57b184b9113151f1090fc1bed30bb
Kazhtova <kasshhhoktora@gmail.com>
2026-09-18 10:03:16 +0700

    Add API Journal

--- backend-api/app/Http/Controllers/Api/JournalController.php
+++ backend-api/app/Http/Controllers/Api/JournalController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\FinancialJournal;
 use Illuminate\Http\Request;
 
 class JournalController extends Controller
 {
     public function index(Request $request){
+        $query = FinancialJournal::with('asset:id,name,qr_code')
+        ->latest('transaction_date')
+        ->orderBy('id', 'desc');
+
         
     }
 }
