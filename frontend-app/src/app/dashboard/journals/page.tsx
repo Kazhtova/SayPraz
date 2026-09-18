4e8ea1173b0643ff832ef607924e6ed7a4923f18
Kazhtova <kasshhhoktora@gmail.com>
2026-09-19 04:44:59 +0700

    Fix Little Bug

--- frontend-app/src/app/dashboard/journals/page.tsx
+++ frontend-app/src/app/dashboard/journals/page.tsx
@@ -89,6 +89,7 @@ export default function JournalsPage() {
   }, [router, currentPage, search, entryTypeFilter]);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     fetchJournals();
   }, [fetchJournals]);
 
